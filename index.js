73530e9c24814efa612c6499fbd464cd8703e864
mariananbp <mariananbp@gmail.com>
2022-01-03 15:51:24 -0300

    Req 1

--- index.js
+++ index.js
@@ -4,14 +4,46 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+/* const HTTP_OK_STATUS = 200;
+const PORT = '3000'; */
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   console.log('Online');
+}); */
+
+const talkers = [
+  {
+    name: 'Henrique Albuquerque',
+    age: 62,
+    id: 1,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Heloísa Albuquerque',
+    age: 67,
+    id: 2,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Ricardo Xavier Filho',
+    age: 33,
+    id: 3,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Marcos Costa',
+    age: 24,
+    id: 4,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+];
+
+app.get('/talker', (req, res) => {
+  if (!talkers) return res.status(200).json([]);
+  res.status(200).json(talkers);
 });
